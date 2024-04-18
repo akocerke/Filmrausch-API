5a2347570df233ed7abcac06e7ce3d2e5c751d98
gabriel <gabrielmasquediez@gmail.com>
2024-04-18 15:40:10 +0200

    noch nicht fertig

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,12 +1,38 @@
 // auth/index.js
-
+const express = require("express");
 const { Router } = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const { AppRouter } = require("..");
 
 const AuthRouter = Router();
-
+AuthRouter.use(bodyParser.json());
 // Beispiel-Endpunkte für die Authentifizierung
 AuthRouter.post("/login", (req, res) => {
-  // Handle login logic
+  const { username, password } = req.body;
+
+  // Consultar la base de datos para verificar las credenciales del usuario
+  const sql = `SELECT * FROM filmrausch WHERE username = ? AND password = ?`;
+  db.query(sql, [username], (err, result) => {
+    if (err) {
+      console.error("Error al consultar la base de datos:", err);
+      res.status(500).send("Error interno del servidor");
+      return;
+    }
+
+    // Verificar si se encontró un usuario con el nombre de usuario proporcionado
+    if (result.length === 0) {
+      res.status(401).send("Nombre de usuario incorrecto");
+      return;
+    }
+
+    // Verificar si la contraseña coincide
+    if (result[0].password === password) {
+      res.status(200).send("Inicio de sesión exitoso");
+    } else {
+      res.status(401).send("Contraseña incorrecta");
+    }
+  });
 });
 
 AuthRouter.post("/register", (req, res) => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { AuthRouter } = require("./auth");
 const { UsersRouter } = require("./users/users");
 // const {FavoritesRouter} = require("./favorites/favorites");
 const { StatusCodes } = require("http-status-codes");
+const bodyParser = require("body-parser");
 
 const AppRouter = Router();
 
 AppRouter.use("/auth", AuthRouter);
 AppRouter.use("/users", UsersRouter);
+AppRouter.use(bodyParser.json());
 // AppRouter.use("/favorites", FavoritesRouter);
 
 module.exports = { AppRouter };
