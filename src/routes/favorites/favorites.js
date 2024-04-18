823fb01d2bbb2be1abee13a2110749e292beeb09
akocerke <152269613+akocerke@users.noreply.github.com>
2024-04-18 13:50:24 +0200

    GET /byUserId/:userId

--- src/routes/favorites/favorites.js
+++ src/routes/favorites/favorites.js
@@ -25,5 +25,27 @@ FavoritesRouter.get("/all", async (req, res) => {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Fehler beim Abrufen aller Favoriten." });
   }
 });
+// GET-Anfrage, um alle Favoriten eines Benutzers anhand der Benutzer-ID zurückzugeben
+FavoritesRouter.get("/byUserId/:userId", async (req, res) => {
+  const userId = req.params.userId;
 
+  try {
+    // Favoriten aus der Datenbank abrufen, die zur Benutzer-ID gehören
+    const userFavorites = await Favorites.findAll({ where: { user_id: userId } });
+
+    if (userFavorites.length === 0) {
+      console.log(`Keine Favoriten gefunden für Benutzer mit der ID ${userId}`);
+      return res.status(StatusCodes.NOT_FOUND).json({ message: `Keine Favoriten gefunden für Benutzer mit der ID ${userId}` });
+    }
+
+    // Extrahiere nur die movie_id aus den Favoriten
+    const movieIds = userFavorites.map(favorite => favorite.movie_id);
+
+    console.log(`Favoriten gefunden für Benutzer mit der ID ${userId}`);
+    res.status(StatusCodes.OK).json({ message: `Favoriten gefunden für Benutzer mit der ID ${userId}`, movieIds: movieIds });
+  } catch (error) {
+    console.error(`Fehler beim Abrufen der Favoriten für Benutzer mit der ID ${userId}:`, error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: `Fehler beim Abrufen der Favoriten für Benutzer mit der ID ${userId}.` });
+  }
+});
 module.exports = FavoritesRouter;
